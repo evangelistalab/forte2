1d271b95eed60c7f4de6a4f0f3a0b095f6a9964a
Brian Zhao <zijun.zhao@emory.edu>
2025-05-07 16:09:59 -0400

    change x2c test to Br2

--- tests/scf/test_x2c1e.py
+++ tests/scf/test_x2c1e.py
@@ -9,22 +9,22 @@ from forte2.scf import RHF, get_hcore_x2c
 
 
 def test_sfx2c1e():
+    escf = -5192.021044046336
     xyz = """
-    O            0.000000000000     0.000000000000    -0.061664597388
-    H            0.000000000000    -0.711620616369     0.489330954643
-    H            0.000000000000     0.711620616369     0.489330954643
+    Br 0 0 0
+    Br 0 0 1.2
     """
 
     system = forte2.System(
         xyz=xyz, basis_name="cc-pVQZ", auxiliary_basis_name="cc-pVQZ-JKFIT"
     )
 
-    scf = RHF(charge=0)
+    scf = RHF(charge=0, econv=1e-10, dconv=1e-8)
     scf._get_hcore = lambda x: get_hcore_x2c(x, x2c_type="sf")
     scf.run(system)
     assert np.isclose(
-        scf.E, -76.110651355917, atol=1e-10
-    ), f"SCF energy {scf.E} is not close to expected value -76.110651355917"
+        scf.E, escf, atol=1e-10
+    ), f"SCF energy {scf.E} is not close to expected value escf"
 
 
 def test_sox2c1e():
