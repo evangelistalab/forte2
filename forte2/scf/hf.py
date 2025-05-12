0dfb5808cd1422bf10cdfa68263e3ce52a0225b1
Brian Zhao <zijun.zhao@emory.edu>
2025-05-12 14:00:38 -0400

    merge main

--- forte2/scf/hf.py
+++ forte2/scf/hf.py
@@ -11,6 +11,12 @@ from .initial_guess import minao_initial_guess
 
 class SCFMixin:
 
+    def _get_hcore(self, system):
+        T = forte2.ints.kinetic(system.basis)
+        V = forte2.ints.nuclear(system.basis, system.atoms)
+        H = T + V
+        return H
+
     def _check_scf_params(self):
         pass
 
@@ -19,7 +25,7 @@ class SCFMixin:
 
     def _check_parameters(self):
         scftyp = self._scf_type()
-        if scftyp == 'RHF':
+        if scftyp == "RHF":
             assert self.na == self.nb
 
     def run_scf(self, system):
@@ -29,7 +35,7 @@ class SCFMixin:
         Zsum = np.sum([x[0] for x in system.atoms])
         nel = Zsum - self.charge
         self.nbasis = system.basis.size
-        self.na = (nel + self.mult - 1) // 2 
+        self.na = (nel + self.mult - 1) // 2
         self.nb = (nel - self.mult + 1) // 2
         self.sz = (self.na - self.nb) / 2.0
 
@@ -49,7 +55,7 @@ class SCFMixin:
 
         Vnn = forte2.ints.nuclear_repulsion(system.atoms)
         S = forte2.ints.overlap(system.basis)
-        
+
         fock_builder = DFFockBuilder(system)
 
         H = self._get_hcore(system)
@@ -60,11 +66,13 @@ class SCFMixin:
         Eold = 0.0
         Dold = self.D
 
-        print(f"{'Iter':>4s} {'Energy':>20s} {'deltaE':>20s} {'|deltaD|':>20s} {'|AO grad|':>20s} {'<S^2>':>20s}")
+        print(
+            f"{'Iter':>4s} {'Energy':>20s} {'deltaE':>20s} {'|deltaD|':>20s} {'|AO grad|':>20s} {'<S^2>':>20s}"
+        )
 
         for iter in range(self.maxiter):
 
-            # 1. Build the Fock matrix 
+            # 1. Build the Fock matrix
             # (there is a slot for canonicalized F to accommodate ROHF and CUHF methods - admittedly weird for RHF/UHF)
             F, F_canon = self._build_fock(H, fock_builder, S)
             # 2. Build the orbital gradient (DIIS residual)
@@ -85,19 +93,19 @@ class SCFMixin:
 
             # print iteration
             print(
-                    f"{iter + 1:4d} {self.E:20.12f} {deltaE:20.12f} {deltaD:20.12f} {np.linalg.norm(AO_grad):20.12f} {spin2:20.12f}"
+                f"{iter + 1:4d} {self.E:20.12f} {deltaE:20.12f} {deltaD:20.12f} {np.linalg.norm(AO_grad):20.12f} {spin2:20.12f}"
             )
 
             if np.abs(deltaE) < self.econv and deltaD < self.dconv:
                 print(f"{method} iterations converged\n")
-                # perform final iteration 
+                # perform final iteration
                 F, F_canon = self._build_fock(H, fock_builder, S)
                 self.eps, self.C = self._diagonalize_fock(F_canon, S)
                 self.D = self._build_density_matrix()
                 self.E = Vnn + self._energy(H, F)
                 print(f"Final {method} Energy: {self.E:20.12f}")
                 break
-            
+
             # reset old parameters
             Eold = self.E
             Dold = self.D
@@ -127,7 +135,7 @@ class RHF(SCFMixin, MOs):
         return F, F
 
     def _build_density_matrix(self):
-        D = np.einsum("mi,ni->mn", self.C[0][:, :self.na], self.C[0][:, :self.na])
+        D = np.einsum("mi,ni->mn", self.C[0][:, : self.na], self.C[0][:, : self.na])
         return [D]
 
     def _initial_guess(self, system, H, S):
@@ -149,7 +157,7 @@ class RHF(SCFMixin, MOs):
         return [eps], [C]
 
     def _spin(self, S):
-        return self.sz*(self.sz + 1)
+        return self.sz * (self.sz + 1)
 
     def _diis_update(self, diis, F, AO_grad):
         return diis.update(F, AO_grad)
@@ -168,7 +176,7 @@ class ROHF(SCFMixin, MOs):
 
     def _build_fock(self, H, fock_builder, S):
         Ja, Jb = fock_builder.build_J(self.D)
-        K = fock_builder.build_K([self.C[0][:, :self.na], self.C[0][:, :self.nb]])
+        K = fock_builder.build_K([self.C[0][:, : self.na], self.C[0][:, : self.nb]])
         F = [H + Ja + Jb - k for k in K]
 
         F_canon = self._build_canonical_fock(F, S)
@@ -200,8 +208,8 @@ class ROHF(SCFMixin, MOs):
         return fock
 
     def _build_density_matrix(self):
-        D_a = np.einsum("mi,ni->mn", self.C[0][:, :self.na], self.C[0][:, :self.na])
-        D_b = np.einsum("mi,ni->mn", self.C[0][:, :self.nb], self.C[0][:, :self.nb])
+        D_a = np.einsum("mi,ni->mn", self.C[0][:, : self.na], self.C[0][:, : self.na])
+        D_b = np.einsum("mi,ni->mn", self.C[0][:, : self.nb], self.C[0][:, : self.nb])
         return D_a, D_b
 
     def _initial_guess(self, system, H, S):
@@ -220,13 +228,13 @@ class ROHF(SCFMixin, MOs):
         return [eps], [C]
 
     def _spin(self, S):
-        return self.sz*(self.sz + 1)
+        return self.sz * (self.sz + 1)
 
     def _energy(self, H, F):
         energy = 0.5 * (
-                np.einsum("vu,uv->", self.D[0] + self.D[1], H)
-                + np.einsum("vu,uv->", self.D[0], F[0])
-                + np.einsum("vu,uv->", self.D[1], F[1])
+            np.einsum("vu,uv->", self.D[0] + self.D[1], H)
+            + np.einsum("vu,uv->", self.D[0], F[0])
+            + np.einsum("vu,uv->", self.D[1], F[1])
         )
         return energy
 
@@ -247,7 +255,7 @@ class UHF(SCFMixin, MOs):
 
     def _build_fock(self, H, fock_builder, S):
         Ja, Jb = fock_builder.build_J(self.D)
-        K = fock_builder.build_K([self.C[0][:, :self.na], self.C[1][:, :self.nb]])
+        K = fock_builder.build_K([self.C[0][:, : self.na], self.C[1][:, : self.nb]])
         F = [H + Ja + Jb - k for k in K]
 
         F_canon = F
@@ -255,8 +263,8 @@ class UHF(SCFMixin, MOs):
         return F, F_canon
 
     def _build_density_matrix(self):
-        D_a = np.einsum("mi,ni->mn", self.C[0][:, :self.na], self.C[0][:, :self.na])
-        D_b = np.einsum("mi,ni->mn", self.C[1][:, :self.nb], self.C[1][:, :self.nb])
+        D_a = np.einsum("mi,ni->mn", self.C[0][:, : self.na], self.C[0][:, : self.na])
+        D_b = np.einsum("mi,ni->mn", self.C[1][:, : self.nb], self.C[1][:, : self.nb])
         return D_a, D_b
 
     def _initial_guess(self, system, H, S):
@@ -270,7 +278,9 @@ class UHF(SCFMixin, MOs):
         return D_a, D_b
 
     def _build_ao_grad(self, S, F):
-        AO_grad = np.hstack([(f @ d @ S - S @ d @ f).flatten() for d, f in zip(self.D, F)])
+        AO_grad = np.hstack(
+            [(f @ d @ S - S @ d @ f).flatten() for d, f in zip(self.D, F)]
+        )
         return AO_grad
 
     def _diagonalize_fock(self, F, S):
@@ -279,27 +289,36 @@ class UHF(SCFMixin, MOs):
         return [eps_a, eps_b], [C_a, C_b]
 
     def _spin(self, S):
-        # alpha-beta orbital overlap matrix 
+        # alpha-beta orbital overlap matrix
         # S_ij = < psi_i | psi_j >, i,j=occ
         #      = \sum_{uv} c_ui^* c_vj <u|v>
-        S_ij = np.einsum("ui,uv,vj->ij", self.C[0][:, :self.na].conj(), S, self.C[1][:, :self.nb], optimize=True)
+        S_ij = np.einsum(
+            "ui,uv,vj->ij",
+            self.C[0][:, : self.na].conj(),
+            S,
+            self.C[1][:, : self.nb],
+            optimize=True,
+        )
         # Spin contamination: <s^2> - <s^2>_exact = N_b - \sum_{ij} |S_ij|^2
         ds2 = self.nb - np.einsum("ij,ij->", S_ij.conj(), S_ij)
         # <S^2> value
-        s2 = self.sz*(self.sz + 1) + ds2
+        s2 = self.sz * (self.sz + 1) + ds2
         return s2
 
     def _energy(self, H, F):
         energy = 0.5 * (
-                np.einsum("vu,uv->", self.D[0] + self.D[1], H)
-                + np.einsum("vu,uv->", self.D[0], F[0])
-                + np.einsum("vu,uv->", self.D[1], F[1])
+            np.einsum("vu,uv->", self.D[0] + self.D[1], H)
+            + np.einsum("vu,uv->", self.D[0], F[0])
+            + np.einsum("vu,uv->", self.D[1], F[1])
         )
         return energy
 
     def _diis_update(self, diis, F, AO_grad):
         F_flat = diis.update(np.hstack([f.flatten() for f in F]), AO_grad)
-        F = [F_flat[:self.nbasis**2].reshape(self.nbasis, self.nbasis), F_flat[self.nbasis**2:].reshape(self.nbasis, self.nbasis)]
+        F = [
+            F_flat[: self.nbasis**2].reshape(self.nbasis, self.nbasis),
+            F_flat[self.nbasis**2 :].reshape(self.nbasis, self.nbasis),
+        ]
         return F
 
 
@@ -316,7 +335,7 @@ class CUHF(SCFMixin, MOs):
 
     def _build_fock(self, H, fock_builder, S):
         Ja, Jb = fock_builder.build_J(self.D)
-        K = fock_builder.build_K([self.C[0][:, :self.na], self.C[1][:, :self.nb]])
+        K = fock_builder.build_K([self.C[0][:, : self.na], self.C[1][:, : self.nb]])
         F = [H + Ja + Jb - k for k in K]
 
         F_canon = self._build_canonical_fock(F, S)
@@ -339,20 +358,20 @@ class CUHF(SCFMixin, MOs):
         def _build_fock_eff(f):
             # these are scaled by 0.5 to account for fock + fock.T below
             fock = _project(U_core, U_core, f) * 0.5  # cc
-            fock += _project(U_open, U_open, f) * 0.5 # oo
-            fock += _project(U_virt, U_virt, f) * 0.5 # vv
-            # off-diagonal blocks 
-            fock += _project(U_open, U_core, f) # oc
-            fock += _project(U_open, U_virt, f) # ov
-            fock += _project(U_virt, U_core, F_cs) # Replace cv sector with fcore
+            fock += _project(U_open, U_open, f) * 0.5  # oo
+            fock += _project(U_virt, U_virt, f) * 0.5  # vv
+            # off-diagonal blocks
+            fock += _project(U_open, U_core, f)  # oc
+            fock += _project(U_open, U_virt, f)  # ov
+            fock += _project(U_virt, U_core, F_cs)  # Replace cv sector with fcore
             fock = fock + fock.conj().T
             return fock
 
         return [_build_fock_eff(f) for f in F]
 
     def _build_density_matrix(self):
-        D_a = np.einsum("mi,ni->mn", self.C[0][:, :self.na], self.C[0][:, :self.na])
-        D_b = np.einsum("mi,ni->mn", self.C[1][:, :self.nb], self.C[1][:, :self.nb])
+        D_a = np.einsum("mi,ni->mn", self.C[0][:, : self.na], self.C[0][:, : self.na])
+        D_b = np.einsum("mi,ni->mn", self.C[1][:, : self.nb], self.C[1][:, : self.nb])
         return D_a, D_b
 
     def _initial_guess(self, system, H, S):
@@ -364,7 +383,9 @@ class CUHF(SCFMixin, MOs):
         return D_a, D_b
 
     def _build_ao_grad(self, S, F):
-        AO_grad = np.hstack([(f @ d @ S - S @ d @ f).flatten() for d, f in zip(self.D, F)])
+        AO_grad = np.hstack(
+            [(f @ d @ S - S @ d @ f).flatten() for d, f in zip(self.D, F)]
+        )
         return AO_grad
 
     def _diagonalize_fock(self, F, S):
@@ -373,25 +394,34 @@ class CUHF(SCFMixin, MOs):
         return [eps_a, eps_b], [C_a, C_b]
 
     def _spin(self, S):
-        # alpha-beta orbital overlap matrix 
+        # alpha-beta orbital overlap matrix
         # S_ij = < psi_i | psi_j >, i,j=occ
         #      = \sum_{uv} c_ui^* c_vj <u|v>
-        S_ij = np.einsum("ui,uv,vj->ij", self.C[0][:, :self.na].conj(), S, self.C[1][:, :self.nb], optimize=True)
+        S_ij = np.einsum(
+            "ui,uv,vj->ij",
+            self.C[0][:, : self.na].conj(),
+            S,
+            self.C[1][:, : self.nb],
+            optimize=True,
+        )
         # Spin contamination: <s^2> - <s^2>_exact = N_b - \sum_{ij} |S_ij|^2
         ds2 = self.nb - np.einsum("ij,ij->", S_ij.conj(), S_ij)
         # <S^2> value
-        s2 = self.sz*(self.sz + 1) + ds2
+        s2 = self.sz * (self.sz + 1) + ds2
         return s2
 
     def _energy(self, H, F):
         energy = 0.5 * (
-                np.einsum("vu,uv->", self.D[0] + self.D[1], H)
-                + np.einsum("vu,uv->", self.D[0], F[0])
-                + np.einsum("vu,uv->", self.D[1], F[1])
+            np.einsum("vu,uv->", self.D[0] + self.D[1], H)
+            + np.einsum("vu,uv->", self.D[0], F[0])
+            + np.einsum("vu,uv->", self.D[1], F[1])
         )
         return energy
 
     def _diis_update(self, diis, F, AO_grad):
         F_flat = diis.update(np.hstack([f.flatten() for f in F]), AO_grad)
-        F = [F_flat[:self.nbasis**2].reshape(self.nbasis, self.nbasis), F_flat[self.nbasis**2:].reshape(self.nbasis, self.nbasis)]
+        F = [
+            F_flat[: self.nbasis**2].reshape(self.nbasis, self.nbasis),
+            F_flat[self.nbasis**2 :].reshape(self.nbasis, self.nbasis),
+        ]
         return F
