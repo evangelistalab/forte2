e6a1c0f10d47306ecac90bcaa1938c25c8759ddd
marink2 <kmarin3@emory.edu>
2025-06-09 13:04:55 -0400

    added more gasci tests

--- tests/ci/test_gasci_rhf.py
+++ tests/ci/test_gasci_rhf.py
@@ -1,4 +1,4 @@
-from numpy import isclose
+from numpy import isclose # type: ignore
 
 from forte2 import *
 
@@ -47,3 +47,95 @@ def test_gasci_rhf_2():
     assert isclose(ci.E[0], -1.089283671174)
     # TODO: Add assertion for second root when the one below is externally verified
     # assert isclose(ci.E[1], -0.671622137375)
+
+def test_gasci_rhf_3():
+    xyz = f"""
+    H  0.000000000000  0.000000000000 -0.375000000000
+    H  0.000000000000  0.000000000000  0.375000000000
+    """
+
+    system = System(xyz=xyz, basis="sto-6g", auxiliary_basis="def2-universal-jkfit")
+
+    rhf = RHF(charge=0, econv=1e-12, guess_type='hcore')(system)
+    ci = CI(
+        orbitals=[[0], [1]],
+        state=State(nel=2, multiplicity=1, ms=0.0),
+        nroot=1,
+        gas_min=[0,0],
+        gas_max=[2,2],
+    )(rhf)
+    ci.run()
+
+    assert isclose(rhf.E, -1.12475114835983)
+    assert isclose(ci.E[0], -1.145766051194)
+
+def test_gasci_rhf_4():
+    xyz = f"""
+    O  0.000000000000  0.000000000000 -0.069592187400
+    H  0.000000000000 -0.783151105291  0.552239257834
+    H  0.000000000000  0.783151105291  0.552239257834
+    """
+
+    system = System(xyz=xyz, basis="6-31G**", auxiliary_basis="def2-universal-jkfit")
+
+    rhf = RHF(charge=0, econv=1e-12, dconv=1e-8, guess_type='hcore')(system)
+    ci = CI(
+        orbitals=[[0,1,2,3,4], [5,6]],
+        state=State(nel=10, multiplicity=1, ms=0.0),
+        nroot=1,
+        gas_min=[7,0],
+        gas_max=[10,4],
+    )(rhf)
+    ci.run()
+
+    assert isclose(rhf.E, -76.01726423763337)
+    assert isclose(ci.E[0], -76.030899030220)
+    # Fails, CI energy does not match RDM energy
+
+def test_gasci_rhf_5():
+    xyz = f"""
+    O  0.000000000000  0.000000000000 -0.069592187400
+    H  0.000000000000 -0.783151105291  0.552239257834
+    H  0.000000000000  0.783151105291  0.552239257834
+    """
+
+    system = System(xyz=xyz, basis="6-31G**", auxiliary_basis="cc-pvdz-jkfit")
+
+    rhf = RHF(charge=0, econv=1e-12, dconv=1e-8, guess_type='hcore')(system)
+    ci = CI(
+        orbitals=[[0], [1,2,3,4,5,6]],
+        state=State(nel=10, multiplicity=1, ms=0.0),
+        nroot=1,
+        gas_min=[0],
+        gas_max=[1],
+    )(rhf)
+    ci.run()
+
+    assert isclose(rhf.E, -76.01725998416885)
+    assert isclose(ci.E[0], -55.841523397373)
+    # Fails, CI energy does not match RDM energy
+
+def test_gasci_rohf_6():
+    xyz = f"""
+    C           -0.055505285387     0.281253495230     0.333445183956
+    H            1.241755444477     0.529009702423     1.928202188113
+    H            0.960863251201    -0.259845503299    -1.606568704186
+    H           -1.849273709593     1.285938489070     0.087436138153
+    H           -0.035109236026    -1.797124531812    -0.544640316685
+    """
+
+    system = System(xyz=xyz, basis="sto-6g", auxiliary_basis="def2-universal-jkfit", unit="bohr")
+
+    rhf = ROHF(charge=1, econv=1e-12, dconv=1e-8, guess_type='hcore')(system)
+    ci = CI(
+        orbitals=[[0], [1,2,3,4,5,6,7,8]],
+        state=State(nel=9, multiplicity=2, ms=0.5),
+        nroot=1,
+        gas_min=[1],
+        gas_max=[1],
+    )(rhf)
+    ci.run()
+
+    assert isclose(rhf.E, -39.66353334247484)
+    assert isclose(ci.E[0], -29.237219037891)
+    # rohf energy does not match
