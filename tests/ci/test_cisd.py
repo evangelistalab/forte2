b80d8b6b957c10778204467efc4e6409196a5e47
Brian Zhao <zijun.zhao@emory.edu>
2025-06-25 10:32:12 -0400

    update cisd number from forte1 - still not matching, also rdm test fail

--- tests/ci/test_cisd.py
+++ tests/ci/test_cisd.py
@@ -3,17 +3,24 @@ from forte2 import *
 from forte2.helpers.comparisons import approx
 
 
-@pytest.mark.xfail(reason="CISD energy does not match RDM energy")
+@pytest.mark.xfail(reason="CISD energy is wrong, also does not match RDM energy")
 def test_cisd_1():
+    escf = -99.9977252002953492
+    ecisd = -100.204959657944
+
     xyz = f"""
     H 0.0 0.0 0.0
     F 0.0 0.0 2.0
     """
-
     system = System(
-        xyz=xyz, basis="cc-pVDZ", auxiliary_basis="cc-pVTZ-JKFIT", unit="bohr"
+        xyz=xyz,
+        basis="cc-pVDZ",
+        auxiliary_basis="cc-pVTZ-JKFIT",
+        auxiliary_basis_mp2="cc-pVTZ-RIFIT",
+        unit="bohr",
     )
     rhf = RHF(charge=0, econv=1e-12)(system)
     ci = CISD()(rhf)
     ci.run()
-    assert ci.E[0] == approx(-100.2050066538116)
+    assert rhf.E == approx(escf)
+    assert ci.E[0] == approx(ecisd)
