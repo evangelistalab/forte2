97727e58f3651444a0b3044e4260f9bde4546775
Brian Zhao <zijun.zhao@emory.edu>
2025-07-11 22:38:37 -0400

    use default conv for sfx2c

--- tests/scf/test_x2c1e.py
+++ tests/scf/test_x2c1e.py
@@ -6,7 +6,7 @@ from forte2.helpers.comparisons import approx, approx_loose
 
 
 def test_sfx2c1e():
-    escf = -5192.021043976520
+    escf = -5192.021043979554
     xyz = """
     Br 0 0 0
     Br 0 0 1.2
@@ -16,7 +16,7 @@ def test_sfx2c1e():
         xyz=xyz, basis="cc-pVQZ", auxiliary_basis="cc-pVQZ-JKFIT", x2c_type="sf"
     )
 
-    scf = RHF(charge=0, econv=1e-12, dconv=1e-10)(system)
+    scf = RHF(charge=0)(system)
     scf.run()
     assert scf.E == approx(escf)
 
