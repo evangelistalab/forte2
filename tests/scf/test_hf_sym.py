2cc1c9815de7fc1f02995c2c23a73b7d9e6ba6d4
kgururangan <kgururangan@head.he.chem.emory.edu>
2025-08-03 16:19:44 -0400

    Separated HF symmetry testing.

---
+++ tests/scf/test_hf_sym.py
@@ -0,0 +1,95 @@
+import forte2
+from forte2.scf import RHF
+from forte2.helpers.comparisons import approx
+
+
+def test_rhf():
+    erhf = -76.061466407195
+    expected_mo_irreps = ["A1", "A1", "B2", "A1", "B1", "A1", "B2", "A1", "B2", "A1", "B1", 
+                          "A1", "B2", "A2", "B1", "A1", "B2", "A1", "B2", "B2", "A1", "A2", 
+                          "B1", "A1", "A1", "B2", "B1", "A1", "A2", "B2", "B1", "A1", "B2", 
+                          "B1", "A2", "A1", "B2", "A2", "A1", "B1", "B2", "A1", "B2", "A1", 
+                          "B2", "A1", "B1", "A2", "B1", "A1", "B2", "A1", "B2", "B1", "A1", 
+                          "A2", "A1", "B2", "B1", "A1", "A2", "B2", "A1", "A2", "B2", "B1", 
+                          "B2", "A2", "B1", "A1", "B2", "A1", "B1", "A2", "B1", "A1", "B2", 
+                          "B2", "A2", "A1", "B2", "A1", "B1", "A1", "A2", "B2", "A1", "B2", 
+                          "B2", "A1", "B1", "A1", "B1", "A2", "A1", "B2", "A2", "B1", "A1", 
+                          "B1", "B2", "A1", "B2", "B1", "A2", "A1", "A1", "B2", "B1", "A1", 
+                          "A2", "B2", "A1", "B2", "A1"]
+
+    xyz = """
+    O            0.000000000000     0.000000000000    -0.061664597388
+    H            0.000000000000    -0.711620616369     0.489330954643
+    H            0.000000000000     0.711620616369     0.489330954643
+    """
+
+    system = forte2.System(
+        xyz=xyz, basis_set="cc-pVQZ", auxiliary_basis_set="cc-pVQZ-JKFIT", point_group="C2v"
+    )
+
+    scf = RHF(charge=0)(system)
+    scf.run()
+    assert scf.E == approx(erhf)
+    assert list(map(str.upper, scf.orbital_symmetries)) == expected_mo_irreps
+
+
+def test_rhf_cbd_d2h():
+    erhf = -153.6511710906
+    expected_mo_irreps =["AG", "B2U", "B3U", "B1G", "AG", "B2U", "B3U", "B1G",
+                         "AG", "B2U", "AG", "B1U", "B3U", "B3G", "B2G", "AG",
+                         "B2U", "B1G", "B3U", "AU", "B2U", "B3U", "B1G", "AG",
+                         "AG", "B1G", "B2U", "B1U", "B3G", "B3U", "B2G", "B2U",
+                         "AG", "AU", "B3U", "B1G", "AG", "B1G", "B2U", "B1U",
+                         "B3U", "B1U", "B2U", "B1G", "B2G", "B3U", "B3G", "AG",
+                         "AG", "B3U", "B1G", "B3G", "AU", "B2U", "B2G", "AG",
+                         "B1U", "B2U", "B3U", "B1G", "AU", "B3G", "B2U", "AG",
+                         "B3U", "B2G", "B1G", "B1G", "AU", "B2U", "B3U", "AG",
+                         "B1G", "B2U", "B3U", "B1G"]
+    # IMPORTANT: Make sure that you build a geometry that has its centroid at the origin!
+    xyz = """
+    C    -1.2916277126       -1.4862694893        0.0000000000
+    C     1.2916277126       -1.4862694893        0.0000000000
+    C    -1.2916277126        1.4862694893        0.0000000000
+    C     1.2916277126        1.4862694893       -0.0000000000
+    H    -2.7546827497       -2.9442264047        0.0000000000
+    H     2.7546827497       -2.9442264047        0.0000000000
+    H    -2.7546827497        2.9442264047        0.0000000000
+    H     2.7546827497        2.9442264047       -0.0000000000
+    """
+
+    system = forte2.System(
+        xyz=xyz, basis_set="cc-pvdz", cholesky_tei=True, cholesky_tol=1e-10, point_group="D2h", unit="bohr"
+    )
+
+    scf = RHF(charge=0)(system)
+    scf.run()
+    assert scf.E == approx(erhf)
+    assert list(map(str.upper, scf.orbital_symmetries)) == expected_mo_irreps
+
+
+# def test_rhf_n2_d2h():
+#     erhf = -108.97510295211279
+#     expected_mo_irreps = ["AG", "B1U", "AG", "B1U", "AG", "B2U", "B3U", "B2G", "B3G", "B1U", "AG", "B2U", "B3U", 
+#                           "AG", "B2G", "B3G", "B1U", "AG", "B1G", "B1U", "B2U", "B3U", "AU", "B1U", "AG", "B2G", 
+#                           "B3G", "B1U", "B2U", "B3U", "AG", "B2G", "B3G", "B1U", "AG", "B1G", "B2U", "B3U", "B2U", 
+#                           "B3U", "AG", "B2G", "B3G", "AG", "B1G", "AU", "B1U", "B2G", "B3G", "B1U", "AU", "B1U", 
+#                           "B2U", "B3U", "AG", "B1U", "B2G", "B3G", "AG", "B1U"]
+#     # IMPORTANT: Make sure that you build a geometry that has its centroid at the origin!
+#     xyz = """
+#     N            0.000000000000     0.000000000000     -0.564000000000
+#     N            0.000000000000     0.000000000000     0.564000000000
+#     """
+
+#     system = forte2.System(
+#         xyz=xyz, basis_set="cc-pvtz", cholesky_tei=True, cholesky_tol=1e-10, point_group="D2h"
+#     )
+
+#     scf = RHF(charge=0)(system)
+#     scf.run()
+#     assert scf.E == approx(erhf)
+#     for i, (irrep1, irrep2) in enumerate(zip(scf.orbital_symmetries, expected_mo_irreps)):
+#         if irrep1.upper() != irrep2:
+#             print(f"{i + 1}  {irrep1}  {irrep2}  e = {scf.eps[0][i]}")
+#     assert list(map(str.upper, scf.orbital_symmetries)) == expected_mo_irreps
+
+# test_rhf_n2_d2h()
