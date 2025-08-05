98b3000e0481ad740842878be2634a88421e0496
kgururangan <kgururangan@head.he.chem.emory.edu>
2025-08-04 23:40:44 -0400

    Fix failing CASSCF test for N2.

--- tests/orbopt/test_casscf.py
+++ tests/orbopt/test_casscf.py
@@ -257,13 +257,13 @@ def test_casscf_n2_cholesky_2():
     system = System(xyz=xyz, basis_set="cc-pVDZ", cholesky_tei=True, cholesky_tol=1e-10)
     rhf = RHF(charge=0, econv=1e-12)(system)
 
-    ci_state = CIStates(
-        active_spaces=[4, 5, 6, 7, 8, 9],
+    mc = MCOptimizer(
+        State(nel=14, multiplicity=1, ms=0.0),
         core_orbitals=[0, 1, 2, 3],
-        states=State(nel=14, multiplicity=1, ms=0.0),
-    )
-    mc = MCOptimizer(ci_state, gconv=1e-7)(rhf)
+        active_orbitals=[4, 5, 6, 7, 8, 9],
+        econv=1e-9,
+    )(rhf)
     mc.run()
     assert rhf.E == approx(erhf)
     assert mc.E == approx(emcscf)
-
+test_casscf_n2_cholesky_2()
