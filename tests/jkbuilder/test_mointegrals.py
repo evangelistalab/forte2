48639d23afd496743a097e3e77add6b49a1cf2ea
Brian Zhao <zijun.zhao@emory.edu>
2025-09-05 09:53:28 -0400

    more integral tests

---
+++ tests/jkbuilder/test_mointegrals.py
@@ -0,0 +1,196 @@
+import numpy as np
+
+from forte2 import System
+from forte2.jkbuilder.mointegrals import RestrictedMOIntegrals, SpinorbitalIntegrals
+from forte2.jkbuilder.jkbuilder import FockBuilder
+
+
+def test_restricted_mo_integrals():
+    xyz = """
+    H 0.0 0.0 0.0
+    F 0.0 0.0 2.0
+    """
+
+    system = System(
+        xyz=xyz,
+        basis_set="cc-pvdz",
+        auxiliary_basis_set="cc-pVTZ-JKFIT",
+        unit="bohr",
+    )
+
+    fock_builder = FockBuilder(system)
+    C = np.random.rand(system.nbf, system.nbf)
+    ints = RestrictedMOIntegrals(
+        system,
+        C,
+        orbitals=list(range(C.shape[1])),
+        fock_builder=fock_builder,
+    )
+    B_mo = np.einsum("Bpq,pi,qj->Bij", fock_builder.B, C.conj(), C, optimize=True)
+    V_ref = np.einsum("Bij,Bkl->ikjl", B_mo, B_mo, optimize=True)
+    assert np.allclose(ints.V, V_ref), np.linalg.norm(ints.V - V_ref)
+
+    ints_antisym = RestrictedMOIntegrals(
+        system,
+        C,
+        orbitals=list(range(C.shape[1])),
+        fock_builder=fock_builder,
+        antisymmetrize=True,
+    )
+    V_ref_antisym = V_ref - V_ref.swapaxes(2, 3)
+    assert np.allclose(ints_antisym.V, V_ref_antisym), np.linalg.norm(
+        ints_antisym.V - V_ref_antisym
+    )
+
+
+def test_spinorbital_integrals():
+    xyz = """
+    H 0.0 0.0 0.0
+    F 0.0 0.0 2.0
+    """
+
+    system = System(
+        xyz=xyz,
+        basis_set="cc-pvdz",
+        auxiliary_basis_set="cc-pVTZ-JKFIT",
+        unit="bohr",
+    )
+    system.two_component = True
+
+    nbf = system.nbf * 2
+    fock_builder = FockBuilder(system)
+    C = np.random.rand(nbf, nbf) + 1j * np.random.rand(nbf, nbf)
+    ints = SpinorbitalIntegrals(
+        system,
+        C,
+        spinorbitals=list(range(C.shape[1])),
+        fock_builder=fock_builder,
+    )
+    B_spinorbital = np.zeros((fock_builder.B.shape[0], nbf, nbf), dtype=complex)
+    B_spinorbital[:, : nbf // 2, : nbf // 2] = fock_builder.B
+    B_spinorbital[:, nbf // 2 :, nbf // 2 :] = fock_builder.B
+    B_mo = np.einsum("Bpq,pi,qj->Bij", B_spinorbital, C.conj(), C, optimize=True)
+    V_ref = np.einsum("Bij,Bkl->ikjl", B_mo, B_mo, optimize=True)
+    assert np.allclose(ints.V, V_ref), np.linalg.norm(ints.V - V_ref)
+
+    ints_antisym = SpinorbitalIntegrals(
+        system,
+        C,
+        spinorbitals=list(range(C.shape[1])),
+        fock_builder=fock_builder,
+        antisymmetrize=True,
+    )
+    V_ref_antisym = V_ref - V_ref.swapaxes(2, 3)
+    assert np.allclose(ints_antisym.V, V_ref_antisym), np.linalg.norm(
+        ints_antisym.V - V_ref_antisym
+    )
+
+
+def test_jkbuilder_general():
+    xyz = """
+    H 0.0 0.0 0.0
+    F 0.0 0.0 2.0
+    """
+
+    system = System(
+        xyz=xyz,
+        basis_set="cc-pvdz",
+        auxiliary_basis_set="cc-pVTZ-JKFIT",
+        unit="bohr",
+    )
+    nmo = system.nbf
+    C = np.random.rand(nmo, nmo)
+    actv = slice(1, 7)
+    Cact = C[:, 1:7]
+    fock_builder = FockBuilder(system)
+    ints = RestrictedMOIntegrals(
+        system, C, orbitals=list(range(C.shape[1])), fock_builder=fock_builder
+    )
+    nact = 6
+    rdm1 = np.random.rand(nact, nact)
+    # make rdm1 hermitian
+    rdm1 += rdm1.conj().T
+    # make rdm1 positive semi-definite
+    rdm1 = rdm1 @ rdm1.T.conj()
+    Jact_ref = np.einsum("ptqu,tu->pq", ints.V[:, actv, :, actv], rdm1, optimize=True)
+    Kact_ref = np.einsum("ptuq,tu->pq", ints.V[:, actv, actv, :], rdm1, optimize=True)
+
+    Jact, Kact = fock_builder.build_JK_generalized(Cact, rdm1)
+    Jact = np.einsum("mp,nq,mn->pq", C.conj(), C, Jact, optimize=True)
+    Kact = np.einsum("mp,nq,mn->pq", C.conj(), C, Kact, optimize=True)
+
+    assert np.allclose(Jact, Jact_ref), np.linalg.norm(Jact - Jact_ref)
+    assert np.allclose(Kact, Kact_ref), np.linalg.norm(Kact - Kact_ref)
+
+
+def test_jkbuilder_complex():
+    xyz = """
+    H 0.0 0.0 0.0
+    F 0.0 0.0 2.0
+    """
+
+    system = System(
+        xyz=xyz,
+        basis_set="cc-pvdz",
+        auxiliary_basis_set="cc-pVTZ-JKFIT",
+        unit="bohr",
+        x2c_type="so",
+    )
+
+    nmo = system.nbf * 2
+    C = np.random.rand(nmo, nmo) + 1j * np.random.rand(nmo, nmo)
+    occ = slice(0, 10)
+    Cocc = C[:, occ]
+
+    fock_builder = FockBuilder(system)
+    ints = SpinorbitalIntegrals(
+        system, C, spinorbitals=list(range(C.shape[1])), fock_builder=fock_builder
+    )
+
+    J_ref = np.einsum("piqi->pq", ints.V[:, occ, :, occ], optimize=True)
+    K_ref = np.einsum("piiq->pq", ints.V[:, occ, occ, :], optimize=True)
+
+    J, K = fock_builder.build_JK([Cocc])
+    J = np.einsum("mp,nq,mn->pq", C.conj(), C, J[0], optimize=True)
+    K = np.einsum("mp,nq,mn->pq", C.conj(), C, K[0], optimize=True)
+
+    assert np.allclose(J, J_ref), np.linalg.norm(J - J_ref)
+    assert np.allclose(K, K_ref), np.linalg.norm(K - K_ref)
+
+
+def test_jkbuilder_general_complex():
+    xyz = """
+    H 0.0 0.0 0.0
+    F 0.0 0.0 2.0
+    """
+
+    system = System(
+        xyz=xyz,
+        basis_set="cc-pvdz",
+        auxiliary_basis_set="cc-pVTZ-JKFIT",
+        unit="bohr",
+        x2c_type="so",
+    )
+    nmo = system.nbf * 2
+    C = np.random.rand(nmo, nmo) + 1j * np.random.rand(nmo, nmo)
+    actv = slice(2, 14)
+    Cact = C[:, 2:14]
+    fock_builder = FockBuilder(system)
+    ints = SpinorbitalIntegrals(
+        system, C, spinorbitals=list(range(C.shape[1])), fock_builder=fock_builder
+    )
+    nact = 12
+    rdm1 = np.random.rand(nact, nact) + 1j * np.random.rand(nact, nact)
+    # make rdm1 hermitian
+    rdm1 += rdm1.conj().T
+    # make rdm1 positive semi-definite
+    rdm1 = rdm1 @ rdm1.T.conj()
+    Jact_ref = np.einsum("ptqu,tu->pq", ints.V[:, actv, :, actv], rdm1, optimize=True)
+    Kact_ref = np.einsum("ptuq,tu->pq", ints.V[:, actv, actv, :], rdm1, optimize=True)
+
+    Jact, Kact = fock_builder.build_JK_generalized(Cact, rdm1)
+    Jact = np.einsum("mp,nq,mn->pq", C.conj(), C, Jact, optimize=True)
+    Kact = np.einsum("mp,nq,mn->pq", C.conj(), C, Kact, optimize=True)
+
+    assert np.allclose(Jact, Jact_ref), np.linalg.norm(Jact - Jact_ref)
+    assert np.allclose(Kact, Kact_ref), np.linalg.norm(Kact - Kact_ref)
