4e9ffe35f1629bc1dc10e0dfcce5916526ad8aa2
fevangelista <francesco.evangelista@emory.edu>
2025-10-29 12:14:14 -0400

    Backward compatibility

--- forte2/__init__.py
+++ forte2/__init__.py
@@ -9,7 +9,7 @@ from .state import State, RelState, MOSpace
 from .scf import RHF, ROHF, UHF, CUHF, GHF
 from .ci import CI, RelCI
 from .x2c import x2c
-from .orbitals import AVAS, CubeGenerator, ASET
+from .orbitals import AVAS, CubeGenerator, Cube, ASET
 from .mcopt import MCOptimizer, RelMCOptimizer
 from .props import get_1e_property, mulliken_population
 from .helpers import logger, set_verbosity_level, comparisons
--- forte2/orbitals/__init__.py
+++ forte2/orbitals/__init__.py
@@ -3,3 +3,6 @@ from .cube_generator import CubeGenerator
 from .semicanonicalizer import Semicanonicalizer
 from .aset import ASET
 from .iao import IAO, IBO
+
+# Backward compatibility: keep the old public name available
+Cube = CubeGenerator
