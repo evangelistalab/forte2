5b6141ab60cc6fd1b8422e03f61252b7b1db0a22
Brian Zhao <zijun.zhao@emory.edu>
2025-11-19 23:10:36 -0500

    libcint 3c2e, fix cross ints, test

--- forte2/integrals/libcint_compute.h
+++ forte2/integrals/libcint_compute.h
@@ -10,7 +10,7 @@
 
 #if FORTE2_USE_LIBCINT
 extern "C" {
-#  include <cint.h>
+#include <cint.h>
 
 int CINTcgto_spheric(const int i, const int* bas);
 int CINTcgto_spinor(const int i, const int* bas);
@@ -47,12 +47,7 @@ np_tensor3_f cint_int2c(CIntorFunc intor, const std::vector<int>& shell_slice, n
 
     std::vector<int> ao_offset(nbas + 1, 0);
     for (int i = 0; i < nbas; ++i) {
-#if FORTE2_USE_LIBCINT
         ao_offset[i + 1] = ao_offset[i] + CINTcgto_spheric(i, bas_data);
-#else
-        (void) bas_data; // silence unused warning if compiled without libcint
-        ao_offset[i + 1] = ao_offset[i];
-#endif
     }
 
     const int nao_i = ao_offset[ish_1] - ao_offset[ish_0];
@@ -66,12 +61,16 @@ np_tensor3_f cint_int2c(CIntorFunc intor, const std::vector<int>& shell_slice, n
     int shells[2];
     int dims[2] = {nao_i, nao_j};
 
-    for (int i = 0; i < nish; ++i) {
-        for (int j = 0; j < njsh; ++j) {
-            shells[0] = i;
+    int shell_offset_i, shell_offset_j;
+
+    for (int i = ish_0; i < ish_1; ++i) {
+        shells[0] = i;
+        shell_offset_i = ao_offset[i] - ao_offset[ish_0];
+        for (int j = jsh_0; j < jsh_1; ++j) {
             shells[1] = j;
+            shell_offset_j = ao_offset[j] - ao_offset[jsh_0];
             // Fortran ordering: i changes fastest
-            auto buf_ij = buf + ao_offset[i] + ao_offset[j] * nao_i;
+            auto buf_ij = buf + shell_offset_i + shell_offset_j * nao_i;
             intor(buf_ij, dims, shells, atm_data, natm, bas_data, nbas, env_data, NULL, NULL);
         }
     }
@@ -99,12 +98,7 @@ np_tensor3_complex_f cint_int2c_spinor(CIntorFuncSpinor intor, const std::vector
 
     std::vector<int> ao_offset(nbas + 1, 0);
     for (int i = 0; i < nbas; ++i) {
-#if FORTE2_USE_LIBCINT
         ao_offset[i + 1] = ao_offset[i] + CINTcgto_spinor(i, bas_data);
-#else
-        (void) bas_data;
-        ao_offset[i + 1] = ao_offset[i];
-#endif
     }
 
     const int nao_i = ao_offset[ish_1] - ao_offset[ish_0];
@@ -118,16 +112,81 @@ np_tensor3_complex_f cint_int2c_spinor(CIntorFuncSpinor intor, const std::vector
     int shells[2];
     int dims[2] = {nao_i, nao_j};
 
-    for (int i = 0; i < nish; ++i) {
-        for (int j = 0; j < njsh; ++j) {
-            shells[0] = i;
+    int shell_offset_i, shell_offset_j;
+
+    for (int i = ish_0; i < ish_1; ++i) {
+        shells[0] = i;
+        shell_offset_i = ao_offset[i] - ao_offset[ish_0];
+        for (int j = jsh_0; j < jsh_1; ++j) {
             shells[1] = j;
+            shell_offset_j = ao_offset[j] - ao_offset[jsh_0];
             // Fortran ordering: i changes fastest
-            auto buf_ij = buf + ao_offset[i] + ao_offset[j] * nao_i;
+            auto buf_ij = buf + shell_offset_i + shell_offset_j * nao_i;
             intor(buf_ij, dims, shells, atm_data, natm, bas_data, nbas, env_data, NULL, NULL);
         }
     }
 
     return ints;
 }
+
+// function to compute three-center integrals
+np_tensor3_f cint_int3c(CIntorFunc intor, const std::vector<int>& shell_slice, np_matrix_int atm,
+                        np_matrix_int bas, np_vector env) {
+    const int ish_0 = static_cast<int>(shell_slice[0]);
+    const int ish_1 = static_cast<int>(shell_slice[1]);
+    const int jsh_0 = static_cast<int>(shell_slice[2]);
+    const int jsh_1 = static_cast<int>(shell_slice[3]);
+    const int ksh_0 = static_cast<int>(shell_slice[4]);
+    const int ksh_1 = static_cast<int>(shell_slice[5]);
+
+    const int nish = ish_1 - ish_0;
+    const int njsh = jsh_1 - jsh_0;
+    const int nksh = ksh_1 - ksh_0;
+
+    int natm = atm.shape(0);
+    int nbas = bas.shape(0);
+
+    auto* atm_data = atm.data();
+    auto* bas_data = bas.data();
+    auto* env_data = env.data();
+
+    std::vector<int> ao_offset(nbas + 1, 0);
+    for (int i = 0; i < nbas; ++i) {
+        ao_offset[i + 1] = ao_offset[i] + CINTcgto_spheric(i, bas_data);
+    }
+
+    const int nao_i = ao_offset[ish_1] - ao_offset[ish_0];
+    const int nao_j = ao_offset[jsh_1] - ao_offset[jsh_0];
+    const int nao_k = ao_offset[ksh_1] - ao_offset[ksh_0];
+
+    auto ints = make_zeros<nb::numpy, double, 3, nb::f_contig>(std::array<size_t, 3>{
+        static_cast<size_t>(nao_i), static_cast<size_t>(nao_j), static_cast<size_t>(nao_k)});
+
+    double* buf = ints.data();
+
+    int shells[3];
+    int dims[3] = {nao_i, nao_j, nao_k};
+
+    int shell_offset_i, shell_offset_j, shell_offset_k;
+
+    for (int i = ish_0; i < ish_1; ++i) {
+        shells[0] = i;
+        shell_offset_i = ao_offset[i] - ao_offset[ish_0];
+        for (int j = jsh_0; j < jsh_1; ++j) {
+            shells[1] = j;
+            shell_offset_j = ao_offset[j] - ao_offset[jsh_0];
+            for (int k = ksh_0; k < ksh_1; ++k) {
+                shells[2] = k;
+                shell_offset_k = ao_offset[k] - ao_offset[ksh_0];
+                // Fortran ordering: i changes fastest
+                auto buf_ijk =
+                    buf + shell_offset_i + shell_offset_j * nao_i + shell_offset_k * nao_i * nao_j;
+                intor(buf_ijk, dims, shells, atm_data, natm, bas_data, nbas, env_data, NULL, NULL);
+            }
+        }
+    }
+
+    return ints;
+}
+
 } // namespace forte2
