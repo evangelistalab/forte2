263ea5827d661475e421cd96cdd6670cbd825ac5
Brian Zhao <zijun.zhao@emory.edu>
2025-12-13 10:11:27 -0500

    pt3

--- forte2/helpers/parallel.h
+++ forte2/helpers/parallel.h
@@ -2,7 +2,7 @@
 #include <thread>
 
 namespace forte2 {
-std::size_t get_num_threads() {
+static std::size_t get_num_threads() {
     return static_cast<std::size_t>(std::max(1u, std::thread::hardware_concurrency()));
 }
 } // namespace forte2
