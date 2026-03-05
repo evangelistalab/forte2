1fe665c887393b55709f59b71cfaf019ed95158e
Brian Zhao <zijun.zhao@emory.edu>
2026-03-05 14:35:19 -0500

    remove diis, change mcopt conv criteria, add avg natorbocc

--- tests/mcopt/test_rel_casscf.py
+++ tests/mcopt/test_rel_casscf.py
@@ -20,7 +20,6 @@ def test_rel_casscf_hf_equivalence_to_nonrel():
         nel=10,
         core_orbitals=2,
         active_orbitals=12,
-        do_diis=False,
         maxiter=200,
     )(scf)
     mc.run()
@@ -50,7 +49,6 @@ def test_rel_casscf_hf_ghf():
         nroots=1,
         core_orbitals=2,
         active_orbitals=12,
-        do_diis=False,
         maxiter=200,
     )(scf)
     mc.run()
@@ -151,7 +149,6 @@ def test_rel_casscf_na_ghf():
         nroots=8,
         core_orbitals=10,
         active_orbitals=8,
-        do_diis=False,
         maxiter=500,
     )(scf)
     mc.run()
