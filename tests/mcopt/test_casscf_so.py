4a346428a7fb46c78e5886abe3ab3fc544b811c9
Brian Zhao <zijun.zhao@emory.edu>
2026-03-22 19:53:04 -0400

    fix: x2c runtime param checking (#146)
    * fix x2c runtime param checking
    
    * fix nbf
    
    * save proj
    
    * update tol for wavenumbers

---
+++ tests/mcopt/test_casscf_so.py
@@ -0,0 +1,48 @@
+import pytest
+from forte2 import (
+    System,
+    AVAS,
+    ROHF,
+    MCOptimizer,
+    State,
+    RelCI,
+)
+from forte2.scf.scf_utils import convert_coeff_spatial_to_spinor
+from forte2.data import EH_TO_WN
+
+
+def test_casscf_so():
+    xyz = """
+    Br 0 0 0
+    """
+
+    system = System(
+        xyz=xyz,
+        basis_set="decon-cc-pVTZ",
+        auxiliary_basis_set="cc-pVQZ-JKFIT",
+        x2c_type="sf",
+    )
+
+    rhf = ROHF(charge=0, ms=0.5)(system)
+    avas = AVAS(
+        selection_method="separate",
+        num_active_docc=3,
+        num_active_uocc=0,
+        subspace=["Br(4s)", "Br(4p)"],
+    )(rhf)
+    mc = MCOptimizer(
+        states=State(nel=35, multiplicity=2, ms=0.5),
+        nroots=3,
+    )(avas)
+    mc.run()
+
+    system.two_component = True
+
+    C_2c = convert_coeff_spatial_to_spinor(system, mc.C)
+    rhf.C = C_2c
+    system.x2c_type = "so"
+    system.snso_type = "row-dependent"
+    ci = RelCI(nel=35, nroots=6, core_orbitals=28, active_orbitals=8)(rhf)
+    ci.run()
+
+    assert (ci.E[4] - ci.E[3]) * EH_TO_WN == pytest.approx(3416.391762052979, abs=1e-4)
