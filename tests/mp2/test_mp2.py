ff4dcf4e5989613554cb37b330b7f1a8536e0a9c
czjCharli <zhijian.chen@emory.edu>
2026-04-01 16:13:17 -0400

    Added memory check & RDM contraction check.

--- tests/mp2/test_mp2.py
+++ tests/mp2/test_mp2.py
@@ -1,13 +1,15 @@
 import time
 import numpy as np
 from forte2 import System
+from forte2.jkbuilder.mointegrals import RestrictedMOIntegrals
 from forte2.scf import RHF, ROHF, UHF
 from forte2.helpers.comparisons import approx
 from forte2.mp.dfmp2 import RHFMP2, ROHFMP2, UHFMP2
 
+# reference values from Psi4 using the cc-pVQZ basis set and the cc-pVQZ-JKFIT auxiliary basis set
+
 
 def test_mp2():
-    # reference values from Psi4 using the cc-pVQZ basis set and the cc-pVQZ-JKFIT auxiliary basis set
     energy_scf = -76.0614664072629836
     energy_mp2 = -76.3710978841482984
 
@@ -51,6 +53,7 @@ def test_mp2():
 
     assert Emp2 == approx(energy_mp2)
 
+
 def test_rhf_mp2():
     erhf = -76.0614664072629
     emp2 = -76.3710978833093
@@ -64,8 +67,17 @@ def test_rhf_mp2():
     mp2 = RHFMP2(compute_1rdm=True, compute_2rdm=True, compute_cumulants=True)(scf)
     mp2.run()
 
+    moints = RestrictedMOIntegrals(system, scf.C[0], list(range(scf.nmo)))
+    Ecore = moints.E
+    H = moints.H
+    V = moints.V
+
+    mp2_rdm_E = mp2.mp2_E_given_rdms(Ecore, H, V, mp2.gamma1_sf, mp2.gamma2_sf)
+
     assert scf.E == approx(erhf)
     assert mp2.E_total == approx(emp2)
+    assert mp2_rdm_E == approx(emp2)
+
 
 def test_h4_rhf_mp2():
     erhf = -1.998839903161
@@ -78,14 +90,14 @@ def test_h4_rhf_mp2():
     """
     system = System(xyz=xyz, basis_set="cc-pVQZ", auxiliary_basis_set="cc-pVQZ-JKFIT")
     scf = RHF(charge=0)(system)
-    mp2 = RHFMP2(compute_1rdm=True)(scf)
+    mp2 = RHFMP2()(scf)
     mp2.run()
 
     assert scf.E == approx(erhf)
     assert mp2.E_total == approx(emp2)
 
+
 def test_singlet_rohf_mp2():
-    # Test the ROHF implementation with a simple example (this is equivalent to RHF)
     erohf = -76.061466407194
     emp2 = -76.37109788330923
     xyz = """
@@ -102,6 +114,7 @@ def test_singlet_rohf_mp2():
     assert scf.E == approx(erohf)
     assert mp2.E_total == approx(emp2)
 
+
 def test_triplet_h2o_rohf_mp2():
     erohf = -75.805109024040
     emp2 = -76.1475807766528
@@ -114,11 +127,12 @@ def test_triplet_h2o_rohf_mp2():
     system = System(xyz=xyz, basis_set="cc-pVQZ", auxiliary_basis_set="cc-pVQZ-JKFIT")
 
     scf = ROHF(charge=0, ms=1)(system)
-    mp2 = ROHFMP2()(scf)
+    mp2 = ROHFMP2(compute_1rdm=True, compute_2rdm=True)(scf)
     mp2.run()
 
     assert scf.E == approx(erohf)
     assert mp2.E_total == approx(emp2)
+
 
 def test_triplet_h2o_uhf_mp2():
     euhf = -75.810772399321
@@ -137,8 +151,9 @@ def test_triplet_h2o_uhf_mp2():
     assert scf.E == approx(euhf)
     assert mp2.E_total == approx(emp2)
 
+
 def test_h2o_uhf_mp2():
-    # euhf = -75.810772399321
+    euhf = -76.061466407177
     emp2 = -76.3710978831473
     xyz = """
     O            0.000000000000     0.000000000000    -0.061664597388
@@ -151,7 +166,5 @@ def test_h2o_uhf_mp2():
     mp2 = UHFMP2()(scf)
     mp2.run()
 
-    # assert scf.E == approx(euhf)
+    assert scf.E == approx(euhf)
     assert mp2.E_total == approx(emp2)
-
-test_h2o_uhf_mp2()
