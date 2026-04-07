6ec02837ac0f7bb7dbaeecab04804990327933cf
Brian Zhao <zijun.zhao@emory.edu>
2026-04-07 17:18:41 -0400

    refactor part 1

--- forte2/scf/__init__.py
+++ forte2/scf/__init__.py
@@ -1,6 +1,6 @@
 from .rhf import RHF
 from .rohf import ROHF
-from .uhf import UHF
+from .uhf import UHF, rohf_to_uhf
 from .cuhf import CUHF
 from .ghf import GHF
 from .scf_utils import convert_coeff_spatial_to_spinor
