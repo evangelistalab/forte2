96e673763caac3191b05dbab2e6d6b3aa14e4914
fevangelista <francesco.evangelista@emory.edu>
2026-04-09 20:59:21 -0400

    Initial implementation

--- forte2/sci/sci_helper_sigma_build.cc
+++ forte2/sci/sci_helper_sigma_build.cc
@@ -3,6 +3,7 @@
 #include "helpers/ndarray.h"
 #include "helpers/np_vector_functions.h"
 #include "helpers/np_matrix_functions.h"
+#include "helpers/spin.h"
 
 #include <future>
 
@@ -12,7 +13,8 @@ namespace forte2 {
 
 namespace {
 
-// A helper function to run a loop in parallel over a given number of indices. The work function is called with the index as argument.
+// A helper function to run a loop in parallel over a given number of indices. The work function is
+// called with the index as argument.
 template <typename WorkFn>
 void run_parallel_indices(size_t count, size_t num_threads, WorkFn&& work) {
     if (num_threads <= 1 || count == 0) {
@@ -94,11 +96,11 @@ void SelectedCIHelper::find_matching_dets(std::span<double> basis, std::span<dou
         for (size_t jj{jstart}; jj < jend; ++jj) {
             const auto idx_j = list.sorted_dets_second_string(jj);
             if (const auto it = i_map.find(idx_j); it != i_map.end()) {
-                // NOTE: when find_matching_dets is called from different threads, 
+                // NOTE: when find_matching_dets is called from different threads,
                 // this increment is contention-free, because each thread is working
-                // on its own alpha string (i), and the sigma updates are on disjoint sets of determinants
-                // because they have different alpha strings
-                // Same logic applies to the other branch of this if statement
+                // on its own alpha string (i), and the sigma updates are on disjoint sets of
+                // determinants because they have different alpha strings Same logic applies to the
+                // other branch of this if statement
                 sigma[it->second] += int_sign * basis[det_permutation[jj]];
             }
         }
@@ -222,7 +224,7 @@ void SelectedCIHelper::H2ab(std::span<double> basis, std::span<double> sigma) co
                             const double sign = sign_p * sign_q * sign_r * sign_s;
                             // Check if the determinant with the new beta string exists
                             if (const auto it = i_map.find(k); it != i_map.end()) {
-                                // See the comment in find_matching_dets about why this increment is 
+                                // See the comment in find_matching_dets about why this increment is
                                 // contention-free when called from different threads
                                 sigma[it->second] += v_pqrs * sign * basis[det_permutation[jj]];
                             }
@@ -324,4 +326,145 @@ np_matrix SelectedCIHelper::compute_sf_1rdm(size_t left_root, size_t right_root)
     return sf_1rdm;
 }
 
+np_matrix SelectedCIHelper::compute_aa_2rdm(size_t left_root, size_t right_root) const {
+    // calculate the number of pairs of orbitals p > q
+    const size_t npairs = (norb_ * (norb_ - 1)) / 2;
+    auto rdm = make_zeros<nb::numpy, double, 2>({npairs, npairs});
+    double* rdm_data = rdm.data();
+
+    const auto first_string_size = ab_list_.first_string_size();
+    const auto& one_hole_first_strings = ab_list_.one_hole_first_strings();
+    // Loop over all unique alpha strings
+    for (size_t i{0}; i < first_string_size; ++i) {
+        const auto& sublist = ab_list_.two_hole_string_list()[i];
+        for (const auto& [p, q, hole_idx, sign_pq] : sublist) { // (p < q)
+            const size_t pq = pair_index_gt(p, q);
+            const auto& inv_sublist = ab_list_.two_hole_string_list_inv()[hole_idx];
+            for (const auto& [r, s, j, sign_rs] : inv_sublist) { // (r < s)
+                const size_t rs = pair_index_gt(r, s);
+                const double sign = sign_pq * sign_rs;
+                rdm_data[pq * npairs + rs] +=
+                    find_matching_dets_1rdm(left_root, right_root, ab_list_, i, j, sign);
+            }
+        }
+    }
+    return rdm;
+}
+
+np_matrix SelectedCIHelper::compute_bb_2rdm(size_t left_root, size_t right_root) const {
+    // calculate the number of pairs of orbitals p > q
+    const size_t npairs = (norb_ * (norb_ - 1)) / 2;
+    auto rdm = make_zeros<nb::numpy, double, 2>({npairs, npairs});
+    double* rdm_data = rdm.data();
+
+    const auto first_string_size = ba_list_.first_string_size();
+    const auto& one_hole_first_strings = ba_list_.one_hole_first_strings();
+    // Loop over all unique alpha strings
+    for (size_t i{0}; i < first_string_size; ++i) {
+        const auto& sublist = ba_list_.two_hole_string_list()[i];
+        for (const auto& [p, q, hole_idx, sign_pq] : sublist) { // (p < q)
+            const size_t pq = pair_index_gt(p, q);
+            const auto& inv_sublist = ba_list_.two_hole_string_list_inv()[hole_idx];
+            for (const auto& [r, s, j, sign_rs] : inv_sublist) { // (r < s)
+                const size_t rs = pair_index_gt(r, s);
+                const double sign = sign_pq * sign_rs;
+                rdm_data[pq * npairs + rs] +=
+                    find_matching_dets_1rdm(left_root, right_root, ba_list_, i, j, sign);
+            }
+        }
+    }
+    return rdm;
+}
+
+np_tensor4 SelectedCIHelper::compute_ab_2rdm(size_t left_root, size_t right_root) const {
+    local_timer timer;
+
+    auto rdm = make_zeros<nb::numpy, double, 4>({norb_, norb_, norb_, norb_});
+    double* rdm_data = rdm.data();
+
+    const auto first_string_size = ab_list_.first_string_size();
+    const auto& det_permutation = ab_list_.det_permutation();
+    // Loop over all unique alpha strings
+    for (size_t i{0}; i < first_string_size; ++i) {
+        const auto& i_map = ab_list_.second_string_to_det_index()[i];
+        const auto& sublist_a = ab_list_.one_hole_first_string_list()[i];
+        for (const auto& [p, hole_idx, sign_p] : sublist_a) {
+            const auto& inv_sublist_a = ab_list_.one_hole_first_string_list_inv()[hole_idx];
+            for (const auto& [q, j, sign_q] : inv_sublist_a) {
+                const auto& [jstart, jend] = ab_list_.range(j);
+                // Loop over all the beta strings with the same alpha string
+                for (size_t jj{jstart}; jj < jend; ++jj) {
+                    const auto idx_j = ab_list_.sorted_dets_second_string(jj);
+                    // Now loop over single excitations in the beta string
+                    const auto& sublist_b = ab_list_.one_hole_second_string_list()[idx_j];
+                    for (const auto& [r, hole_idx_b, sign_r] : sublist_b) {
+                        const auto& inv_sublist_b =
+                            ab_list_.one_hole_second_string_list_inv()[hole_idx_b];
+                        for (const auto& [s, k, sign_s] : inv_sublist_b) {
+                            const double sign = sign_p * sign_q * sign_r * sign_s;
+                            // Check if the determinant with the new beta string exists
+                            if (const auto it = i_map.find(k); it != i_map.end()) {
+                                rdm_data[p * norb3_ + r * norb2_ + q * norb_ + s] +=
+                                    sign * c_[nroots_ * it->second + left_root] *
+                                    c_[nroots_ * det_permutation[jj] + right_root];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return rdm;
+}
+
+np_tensor4 SelectedCIHelper::compute_sf_2rdm(size_t left_root, size_t right_root) const {
+    auto rdm_sf = make_zeros<nb::numpy, double, 4>({norb_, norb_, norb_, norb_});
+
+    if (norb_ < 1) {
+        return rdm_sf; // No 2-RDM for less than 1 orbitals
+    }
+
+    auto rdm_sf_v = rdm_sf.view();
+    // Mixed-spin contribution (1 orbital or more)
+    {
+        auto rdm_ab = compute_ab_2rdm(left_root, right_root);
+        auto rdm_ab_v = rdm_ab.view();
+        for (size_t p{0}; p < norb_; ++p) {
+            for (size_t q{0}; q < norb_; ++q) {
+                for (size_t r{0}; r < norb_; ++r) {
+                    for (size_t s{0}; s < norb_; ++s) {
+                        rdm_sf_v(p, q, r, s) += rdm_ab_v(p, q, r, s) + rdm_ab_v(q, p, s, r);
+                    }
+                }
+            }
+        }
+    }
+
+    if (norb_ < 2) {
+        return rdm_sf; // No same-spin contributions to the 2-RDM for less than 2 orbitals
+    }
+
+    // To reduce the  memory footprint, we compute the aa and bb contributions in a packed
+    // format and one at a time.
+    for (auto spin : {Spin::Alpha, Spin::Beta}) {
+        auto rdm_ss = spin == Spin::Alpha ? compute_aa_2rdm(left_root, right_root)
+                                          : compute_bb_2rdm(left_root, right_root);
+        auto rdm_ss_v = rdm_ss.view();
+        for (size_t p{1}, pq{0}; p < norb_; ++p) {
+            for (size_t q{0}; q < p; ++q, ++pq) { // p > q
+                for (size_t r{1}, rs{0}; r < norb_; ++r) {
+                    for (size_t s{0}; s < r; ++s, ++rs) { // r > s
+                        auto element = rdm_ss_v(pq, rs);
+                        rdm_sf_v(p, q, r, s) += element;
+                        rdm_sf_v(q, p, r, s) -= element;
+                        rdm_sf_v(p, q, s, r) -= element;
+                        rdm_sf_v(q, p, s, r) += element;
+                    }
+                }
+            }
+        }
+    }
+
+    return rdm_sf;
+}
 } // namespace forte2
