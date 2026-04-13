d1da732a4159f669ba9f4f32b95d51fa5b35c0b3
Brian Zhao <zijun.zhao@emory.edu>
2026-04-13 14:04:01 -0400

    failling gasci tdm test

--- tests/sparse/test_sparse_rdms.py
+++ tests/sparse/test_sparse_rdms.py
@@ -13,9 +13,10 @@ from forte2 import (
     cpp_helpers,
 )
 from forte2.helpers.comparisons import approx
+from forte2.base_classes import DavidsonLiuParams
 
 
-def test_ci_tdm():
+def test_ci_tdm_same_solver():
     xyz = """
     N 0.0 0.0 -1.0
     N 0.0 0.0 1.0
@@ -64,3 +65,42 @@ def test_ci_tdm():
             assert np.allclose(tdm2_aa, tdm2_aa_ref)
             assert np.allclose(tdm2_ab, tdm2_ab_ref)
             assert np.allclose(tdm2_bb, tdm2_bb_ref)
+
+
+def test_gasci_tdm():
+    xyz = """
+    O  0.000000000000  0.000000000000 -0.069592187400
+    H  0.000000000000 -0.783151105291  0.552239257834
+    H  0.000000000000  0.783151105291  0.552239257834
+    """
+
+    system = System(
+        xyz=xyz, basis_set="cc-pvdz", auxiliary_basis_set="def2-universal-jkfit"
+    )
+
+    rhf = RHF(charge=0, econv=1e-12, dconv=1e-8)(system)
+    ci = CI(
+        active_orbitals=[[0], [2, 3, 4, 5, 6]],
+        states=[
+            State(nel=10, multiplicity=1, ms=0.0, gas_min=[1], gas_max=[1]),
+            State(nel=10, multiplicity=1, ms=0.0, gas_min=[2], gas_max=[2]),
+        ],
+        nroots=[1, 1],
+    )(rhf)
+    ci.run()
+
+    left_solver = ci.sub_solvers[0]
+    right_solver = ci.sub_solvers[1]
+    left_sb = left_solver.ci_sigma_builder
+    right_sb = right_solver.ci_sigma_builder
+
+    C_left = left_solver.csf_C_to_det_C(left_solver.evecs[:, 0])
+    C_right = right_solver.csf_C_to_det_C(right_solver.evecs[:, 0])
+
+    state_left = left_solver.ci_sigma_builder.make_sparse_state(C_left)
+    state_right = right_solver.ci_sigma_builder.make_sparse_state(C_right)
+
+    a_1trdm = left_sb.a_1trdm(right_sb, C_left, C_right)
+    a_1trdm_ref = compute_a_1rdm(state_left, state_right, 6)
+
+    assert np.allclose(a_1trdm, a_1trdm_ref)
