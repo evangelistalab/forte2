c7c7e54c0ede42e35a9aacba6ca5cb5592caace2
Brian Zhao <zijun.zhao@emory.edu>
2026-04-23 22:53:48 -0400

    merge wicked contractions into common

--- forte2/dsrg/utils.py
+++ forte2/dsrg/utils.py
@@ -20,6 +20,59 @@ def antisymmetrize_2body(T, indices):
     return T_anti
 
 
+def hermitize_and_antisymmetrize_two_body_dense(T):
+    # antisymmetrize the residual
+    T += np.einsum(
+        "ijab->abij", T.conj()
+    )  # This is the Hermitized version (i.e., [H,A]), which should then be antisymmetrized
+    temp = T.copy()
+    T -= np.einsum("ijab->jiab", temp)
+    T += np.einsum("ijab->jiba", temp)
+    T -= np.einsum("ijab->ijba", temp)
+
+
+def hermitize_and_antisymmetrize_two_body(T):
+    blks = set(T.keys())
+    # Hermitize first
+    for blk in T.keys():
+        if blk not in blks:
+            continue
+        herm_blk = blk[2:] + blk[:2]
+        if herm_blk in T.keys():
+            temp = T[blk].copy()
+            T[blk] += T[herm_blk].transpose(2, 3, 0, 1).conj()
+            T[herm_blk] += temp.transpose(2, 3, 0, 1).conj()
+            blks.remove(blk)
+            blks.remove(herm_blk)
+
+    for blk in T.keys():
+        ij_same = blk[0] == blk[1]
+        kl_same = blk[2] == blk[3]
+        if not (ij_same or kl_same):
+            continue
+        temp = T[blk].copy()
+        if ij_same:
+            T[blk] -= temp.transpose(1, 0, 2, 3)
+        if kl_same:
+            T[blk] -= temp.transpose(0, 1, 3, 2)
+        if ij_same and kl_same:
+            T[blk] += temp.transpose(1, 0, 3, 2)
+
+
+def hermitize_one_body(T):
+    blks = set(T.keys())
+    for blk in T.keys():
+        if blk not in blks:
+            continue
+        herm_blk = blk[1] + blk[0]
+        if herm_blk in T.keys():
+            temp = T[blk].T.conj()
+            T[blk] += T[herm_blk].T.conj()
+            T[herm_blk] += temp
+            blks.remove(blk)
+            blks.remove(herm_blk)
+
+
 def cas_energy_given_RDMs(E_core, H_cas, V_cas, gamma1, gamma2):
     r"""
     Return the CAS energy.
