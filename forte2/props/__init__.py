3302d7d8fa8ab7d0ac7d6d9bb14f5846a82cd5b4
czjCharli <zhijian.chen@emory.edu>
2026-04-30 16:25:54 -0400

    Fast Mpq, spin-resolved cumulants, spin-free cumulants, tests

--- forte2/props/__init__.py
+++ forte2/props/__init__.py
@@ -1,2 +1,2 @@
 from .props import get_1e_property, mulliken_population, iao_partial_charge
-from .mutual_correlation import MutualCorrelationAnalysis, UMP2MPQFast
+from .mutual_correlation import MutualCorrelationAnalysis, UMP2MPQFast, RMP2MPQFast
