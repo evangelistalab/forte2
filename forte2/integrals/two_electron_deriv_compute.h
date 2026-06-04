f7c46823dac2b0e377a2326513f21abce529c798
fevangelista <francesco.evangelista@emory.edu>
2026-06-04 14:41:34 -0700

    Add comments

--- forte2/integrals/two_electron_deriv_compute.h
+++ forte2/integrals/two_electron_deriv_compute.h
@@ -28,6 +28,25 @@ inline auto shell_to_center_index(const Basis& basis) -> std::vector<std::size_t
     return shell_to_center;
 }
 
+/// @brief Contracts a given set of weights with the derivative of the three-center
+/// two-electron integrals (P| mu nu) with repect to nuclear coordinates, where P is a basis
+/// function from basis1, and mu, nu are basis functions from basis2 and basis3 respectively.
+/// @tparam Weights a contigous array type that can be viewed as a 3D array with shape
+/// (nbasis1_slice, nbasis2_slice, nbasis3_slice)
+/// @tparam Op the libint2 operator to compute the integral for
+/// @param basis1 The first basis set (P)
+/// @param basis2 The second basis set (mu)
+/// @param basis3 The third basis set (nu)
+/// @param shell_slices an array of pairs of (first,last) shell indices to compute for each basis.
+/// @param weights the weights to contract with the integral derivatives, with shape (nbasis1_slice,
+/// nbasis2_slice, nbasis3_slice),
+/// @param charges the list of nuclear charges and coordinates, which must be in the same order as
+/// the centers in the basis sets. This is only used if the operator is position-dependent (e.g.
+/// nuclear attraction), and can be an empty vector for position-independent operators (e.g.
+/// kinetic).
+/// @return a vector of length 3N, where N is the number of atoms, containing the gradient of the
+/// contracted integral with respect to the nuclear coordinates, ordered as (d/dx1, d/dy1,
+/// d/dz1,...).
 template <libint2::Operator Op, typename Weights>
 [[nodiscard]] auto compute_two_electron_3c_deriv_by_shell(
     const Basis& basis1, const Basis& basis2, const Basis& basis3,
@@ -145,6 +164,8 @@ template <libint2::Operator Op, typename Weights>
     return grad;
 }
 
+/// @brief Convenience wrapper around compute_two_electron_3c_deriv_by_shell that computes the
+/// derivative for all shells in each basis set and contracts with the given weights.
 template <libint2::Operator Op, typename Weights>
 [[nodiscard]] auto
 compute_two_electron_3c_deriv(const Basis& basis1, const Basis& basis2, const Basis& basis3,
@@ -160,6 +181,24 @@ compute_two_electron_3c_deriv(const Basis& basis1, const Basis& basis2, const Ba
         weights, charges);
 }
 
+/// @brief Contracts a given set of weights with the derivative of the two-center
+/// two-electron integrals (P|Q) with repect to nuclear coordinates, where P and Q are basis
+/// functions from basis1 and basis2 respectively.
+/// @tparam Weights a contigous array type that can be viewed as a 2D array with shape
+/// (nbasis1_slice, nbasis2_slice)
+/// @tparam Op the libint2 operator to compute the integral for
+/// @param basis1 The first basis set (P)
+/// @param basis2 The second basis set (Q)
+/// @param shell_slices an array of pairs of (first,last) shell indices to compute for each basis.
+/// @param weights the weights to contract with the integral derivatives, with shape (nbasis1_slice,
+/// nbasis2_slice),
+/// @param charges the list of nuclear charges and coordinates, which must be in the same order as
+/// the centers in the basis sets. This is only used if the operator is position-dependent (e.g.
+/// nuclear attraction), and can be an empty vector for position-independent operators (e.g.
+/// kinetic).
+/// @return a vector of length 3N, where N is the number of atoms, containing the gradient of the
+/// contracted integral with respect to the nuclear coordinates, ordered as (d/dx1, d/dy1,
+/// d/dz1,...).
 template <libint2::Operator Op, typename Weights>
 [[nodiscard]] auto compute_two_electron_2c_deriv_by_shell(
     const Basis& basis1, const Basis& basis2,
@@ -257,6 +296,8 @@ template <libint2::Operator Op, typename Weights>
     return grad;
 }
 
+/// @brief Convenience wrapper around compute_two_electron_2c_deriv_by_shell that computes the
+/// derivative for all shells in each basis set and contracts with the given weights.
 template <libint2::Operator Op, typename Weights>
 [[nodiscard]] auto
 compute_two_electron_2c_deriv(const Basis& basis1, const Basis& basis2, const Weights& weights,
