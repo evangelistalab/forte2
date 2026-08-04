01e11a5c09a2a354d9ca027f6773818022aba47c
Zijun Zhao <zijun.zhao@emory.edu>
2026-08-04 14:07:23 -0700

    feat(rel-sci): two-component selected CI solver

--- forte2/lib/rdms.pyi
+++ forte2/lib/rdms.pyi
@@ -49,3 +49,13 @@ def compute_abbb_4rdm(state_left: forte2.lib.sparse_ops.SparseState, state_right
 
 def compute_bbbb_4rdm(state_left: forte2.lib.sparse_ops.SparseState, state_right: forte2.lib.sparse_ops.SparseState, norb: int) -> Annotated[NDArray[numpy.float64], dict(shape=(None, None, None, None, None, None, None, None))]:
     """Compute the beta-beta-beta-beta 4-RDM between two SparseStates"""
+
+def compute_a_1rdm_complex(state_left: forte2.lib.sparse_ops.SparseState, state_right: forte2.lib.sparse_ops.SparseState, norb: int) -> Annotated[NDArray[numpy.complex128], dict(shape=(None, None))]:
+    """
+    Compute the complex alpha 1-RDM between two SparseStates (conjugates the bra)
+    """
+
+def compute_aa_2rdm_complex(state_left: forte2.lib.sparse_ops.SparseState, state_right: forte2.lib.sparse_ops.SparseState, norb: int) -> Annotated[NDArray[numpy.complex128], dict(shape=(None, None, None, None))]:
+    """
+    Compute the complex alpha-alpha 2-RDM between two SparseStates (conjugates the bra)
+    """
