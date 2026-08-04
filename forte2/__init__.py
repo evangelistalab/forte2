1f7a246744b2d297560a465a39faea8115d0dd6f
Zijun Zhao <zijun.zhao@emory.edu>
2026-08-04 14:07:23 -0700

    feat(rel-sci): two-component selected CI solver

--- forte2/__init__.py
+++ forte2/__init__.py
@@ -7,6 +7,7 @@ from .system import System, ModelSystem, HubbardModel
 from .state import State, RelState, MOSpace
 from .scf import RHF, ROHF, UHF, CUHF, GHF
 from .ci import CI, RelCI, CISolver, RelCISolver
+from .sci import SelectedCI, SelectedCISolver, RelSelectedCI, RelSelectedCISolver
 from .x2c import x2c
 from .orbitals import (
     AVAS,
